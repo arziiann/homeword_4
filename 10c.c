061094efa1a751b983d7776ce4adea824e69e71e
arziiann <127401577+arziiann@users.noreply.github.com>
2023-03-20 18:47:38 +0400

    Add files via upload

---
+++ 10c.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int strlen_1(const char* str);
+
+int main()
+{
+	char array[] = "text";
+	printf("%i",  strlen_1(array));
+}
+
+int strlen_1(const char* str)
+{
+	int count = 0;
+	for(char i = 0; str[i] != '\0'; i++)
+	{
+		count++;
+	}
+	return count;
+
+
+}
